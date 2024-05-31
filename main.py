741ae9fc8dd91448233fb293f154faa0ec4d1fc4
SamJohn04 <samuel.john.codes@gmail.com>
2024-05-31 10:41:12 +0530

    fix(tkinter): frame added to root for padding

--- main.py
+++ main.py
@@ -13,30 +13,13 @@ def is_numeric(value: str):
     except:
         return False
 
-# def get_input(prompt: str):
-#     result = []
-#     input_frame = Frame(root, padx=50, pady=50)
-#     input_label = Label(input_frame, text=prompt)
-#     input_label.pack()
-#     input_entry = Entry(input_frame)
-#     input_entry.pack()
-#     input_frame.pack()
-
-#     def submit():
-#         result.append(input_entry.get())
-#         input_frame.destroy()
-
-#     submit_button = Button(input_frame, text='Submit', command=submit, state=['disabled' if is_numeric(input_entry) else 'normal'])
-#     submit_button.pack()
-#     print(result[0])
-#     return result[0]
-
 def load_data():
     data = pd.read_csv('data.csv')
     return data.drop('DEATH_EVENT', axis=1)
 
 def get_input_data(data: DataFrame):
     root = Tk()
+    root.title('Heart Failure Prediction')
     results = []
     entries = []
     input_frame = Frame(root, padx=50, pady=50)
@@ -78,7 +61,10 @@ def main():
 
 if __name__ == '__main__':
     prediction = main()
-    root = Tk(padx=50, pady=50)
-    result_label = Label(root, text=f'Prediction: {"High probability of death" if prediction else "Low probability of death"}')
+    root = Tk()
+    root.title('Heart Failure Prediction')
+    input_frame = Frame(root, padx=50, pady=50)
+    input_frame.pack()
+    result_label = Label(input_frame, text=f'Prediction: {"High probability of death" if prediction else "Low probability of death"}')
     result_label.pack()
     root.mainloop()
